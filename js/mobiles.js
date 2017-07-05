48f414712b2ce64814db5896805edce044e9fdf0
Anuj Aggarwal <anuj.aggarwal63@gmail.com>
2017-07-06 00:18:08 +0530

    Added Price Filter Option and a change in sidebar
    - Added Price Filter Option
    - Changed background colors for Sort and Filter
    - Changed Default order of Items: in Ascending Order
    - Updating Price Filters on Load

--- js/mobiles.js
+++ js/mobiles.js
@@ -7,32 +7,17 @@ var cartItems = {}; // Cart as an object with Data-ID:quantity pairs
 // Window.OnLoad
 $(function () {
     itemsList = $("#items");
+
     fetchCart();
     fetchCatalog(catalog);
 
-    $(document).on('change', '#sort-select', sortCatalog);
+    $(document).on('change', '#sort-select', function(){showCatalog(catalog)});
+    $('#price-filter-button').on('click', function(){showCatalog(catalog);});
 
 });
 
-// Sorts the Catalog based on value of the Sort-Select(event.target)
-function sortCatalog(event) {
-    var id = Number($(event.target)[0].value);
-    // Sort in Ascending Order
-    if (id == 0) {
-        showCatalog(catalog.sort(function (item1, item2) {
-            return item1.price - item2.price;
-        }));
-    }
-    // Sort in descending order
-    else if (id == 1) {
-        showCatalog(catalog.sort(function (item1, item2) {
-            return item2.price - item1.price;
-        }));
-
-    }
-}
 // Add the Item in event.target's Card to the catalog
-function addToCart(event){
+function addToCart(event) {
     var idToAdd = $(event.target).closest("[data-id]").attr("data-id");
     console.log(idToAdd);
     if (cartItems[idToAdd]) {   // If Item already in Cart
@@ -47,22 +32,67 @@ function addToCart(event){
 }
 
 
-
-// Get the catalog from mobiles.json
+// Get the catalog from mobiles.json,
+// update minimum and maximum Prices in Price Filter
 function fetchCatalog() {
     $.getJSON("data/mobiles.json", function (data) {
         catalog = data;
+        // Find minimum Price and update min-price
+        $("#min-price").attr('value', catalog.reduce(function(a, b){
+            if(a==catalog[0]){
+                return Math.min(a.price, b.price);
+            }
+            else{
+                return Math.min(a, b.price);
+            }
+        }));
+        // Find maximum Price and update max-price
+        $("#max-price").attr('value', catalog.reduce(function(a, b){
+            if(a==catalog[0]){
+                return Math.max(a.price, b.price);
+            }
+            else{
+                return Math.max(a, b.price);
+            }
+        }));
+
+        // Show the Catalog
         showCatalog(catalog);
     });
 }
-// Show the catalog passed
+// Sort, Filter and Show the catalog passed
 function showCatalog(catalog) {
+
+    // SORT THE CATALOG
+    var id = $("#sort-select").find('option:selected').attr('value');
+    if (id == 0) {    // Sort in Ascending Order
+        catalog = catalog.sort(function (item1, item2) {
+            return item1.price - item2.price;
+        });
+    }
+    else if (id == 1) {    // Sort in descending order
+        catalog =  catalog.sort(function (item1, item2) {
+            return item2.price - item1.price;
+        });
+    }
+
+    // APPLY THE FILTERS
+    var minPrice = Number($("#min-price").val());
+    var maxPrice = Number($("#max-price").val());
+
+    catalog = catalog.filter(function(item) {
+        return item.price >= minPrice && item.price <= maxPrice;
+    });
+
+
+    // Show the Sorted and Filtered Catalog
     itemsList.html("");
     for (var item of catalog) {
         addItemToList(item);
     }
     $(".addCart").click(addToCart);
 }
+
 // Add passed item to the List of items
 function addItemToList(item) {
     var newItem = $("<div class='col-sm-6 col-lg-4'>");
@@ -83,10 +113,12 @@ function addItemToList(item) {
     itemsList.append(newItem);
 }
 
+
+
 // Get the Cart from the local Storage
 function fetchCart() {
     var cart = JSON.parse(localStorage.getItem('cart'));
-    if(cart){
+    if (cart) {
         cartItems = cart;
     }
 }
